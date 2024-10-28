146c6ee1a777e32887a74e2728fbbc2a5ad7509b
Peter Verveer <pieter.verveer@tno.nl>
2024-10-28 09:29:45 +0000

    Update to use the new set step

--- src/ktwo/_parse_config.py
+++ src/ktwo/_parse_config.py
@@ -7,10 +7,10 @@ from typing import TYPE_CHECKING
 from everest.config import EverestConfig
 from everest.optimizer.everest2ropt import everest2ropt
 from pydantic import BaseModel, ConfigDict
-from ropt.plugins.plan.base import PlanStep
+from ropt.plan import RunStep
 
 if TYPE_CHECKING:
-    from ropt.config.plan import StepConfig
+    from ropt.config.plan import RunStepConfig
     from ropt.plan import Plan
 
 
@@ -32,10 +32,10 @@ class K2ParseEverestConfigStepWith(BaseModel):
     )
 
 
-class K2ParseEverestConfigStep(PlanStep):
+class K2ParseEverestConfigStep(RunStep):
     """The default parse config step."""
 
-    def __init__(self, config: StepConfig, plan: Plan) -> None:
+    def __init__(self, config: RunStepConfig, plan: Plan) -> None:
         """Initialize a default parse config step.
 
         Args:
--- src/ktwo/plugins.py
+++ src/ktwo/plugins.py
@@ -5,15 +5,15 @@ from __future__ import annotations
 from functools import singledispatchmethod
 from typing import Dict, Final, Type, Union
 
-from ropt.config.plan import ResultHandlerConfig, StepConfig  # noqa: TCH002
-from ropt.plan import Plan  # noqa: TCH002
-from ropt.plugins.plan.base import PlanPlugin, PlanStep, ResultHandler
+from ropt.config.plan import ResultHandlerConfig, RunStepConfig  # noqa: TCH002
+from ropt.plan import Plan, ResultHandler, RunStep  # noqa: TCH002
+from ropt.plugins.plan.base import PlanPlugin
 
 from ._parse_config import K2ParseEverestConfigStep
 
 _RESULT_HANDLER_OBJECTS: Final[Dict[str, Type[ResultHandler]]] = {}
 
-_STEP_OBJECTS: Final[Dict[str, Type[PlanStep]]] = {
+_STEP_OBJECTS: Final[Dict[str, Type[RunStep]]] = {
     "parse_everest_config": K2ParseEverestConfigStep,
 }
 
@@ -24,9 +24,9 @@ class K2PlanPlugin(PlanPlugin):
     @singledispatchmethod
     def create(  # type: ignore[override]
         self,
-        config: Union[ResultHandlerConfig, StepConfig],
+        config: Union[ResultHandlerConfig, RunStepConfig],
         plan: Plan,
-    ) -> Union[ResultHandler, PlanStep]:
+    ) -> Union[ResultHandler, RunStep]:
         """Initialize the plan plugin.
 
         See the [ropt.plugins.plan.base.PlanPlugin][] abstract base class.
@@ -49,7 +49,7 @@ class K2PlanPlugin(PlanPlugin):
         raise TypeError(msg)
 
     @create.register
-    def _create_step(self, config: StepConfig, plan: Plan) -> PlanStep:
+    def _create_step(self, config: RunStepConfig, plan: Plan) -> RunStep:
         _, _, step_name = config.run.lower().rpartition("/")
         step_obj = _STEP_OBJECTS.get(step_name)
         if step_obj is not None:
