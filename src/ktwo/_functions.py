17436898481d650f4fed68a8d2e65a03403da665
Peter Verveer <pieter.verveer@tno.nl>
2024-11-13 16:21:39 +0000

    Update everest2ropt function

--- src/ktwo/_functions.py
+++ src/ktwo/_functions.py
@@ -6,8 +6,9 @@ from typing import Any, Dict
 
 from everest.config import EverestConfig
 from everest.optimizer.everest2ropt import everest2ropt
+from ropt.config.enopt import EnOptConfig
 
 
 def fnc_everest2ropt(everest_config: Dict[str, Any]) -> Dict[str, Any]:
     everest_config = EverestConfig.model_validate(everest_config)
-    return everest2ropt(everest_config)
+    return EnOptConfig.model_validate(everest2ropt(everest_config))
