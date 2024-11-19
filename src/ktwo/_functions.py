53e83c279607ebb83d4146e7ba96760097436436
Peter Verveer <pieter.verveer@tno.nl>
2024-11-19 12:51:34 +0000

    Update everest2ropt function

--- src/ktwo/_functions.py
+++ src/ktwo/_functions.py
@@ -2,13 +2,15 @@
 
 from __future__ import annotations
 
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 from everest.config import EverestConfig
 from everest.optimizer.everest2ropt import everest2ropt
-from ropt.config.enopt import EnOptConfig
+
+if TYPE_CHECKING:
+    from ropt.config.enopt import EnOptConfig
 
 
 def fnc_everest2ropt(everest_config: dict[str, Any]) -> EnOptConfig:
     everest_config = EverestConfig.model_validate(everest_config)
-    return EnOptConfig.model_validate(everest2ropt(everest_config))
+    return everest2ropt(everest_config)
