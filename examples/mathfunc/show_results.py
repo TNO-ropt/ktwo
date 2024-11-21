5aaf059968009695f7c4798cbef470b2b597f8da
Peter Verveer <pieter.verveer@tno.nl>
2024-11-21 19:42:12 +0000

    Update mathfunc example

--- examples/mathfunc/show_results.py
+++ examples/mathfunc/show_results.py
@@ -1,10 +1,5 @@
-import pickle  # noqa: D100, INP001
-
-from ropt.config.enopt import EnOptConfig
+# ruff: noqa
 from ropt.results import FunctionResults
 
 results = FunctionResults.from_netcdf("everest_output/results/result00.nc")
-with open("everest_output/results/config.pickle", "rb") as file_obj:  # noqa: PTH123
-    config_dict = pickle.load(file_obj)  # noqa: S301
-config = EnOptConfig.model_validate(config_dict["ropt_config"])
-print(results.to_dataframe(config, "evaluations"))
+print(results.to_dataframe("evaluations"))
